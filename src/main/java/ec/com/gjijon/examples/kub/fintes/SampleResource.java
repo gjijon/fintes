1b107e6a7e334a7b99d9e2540ca014d111b48680
gjijon <gjijon@gmail.com>
2022-12-26 17:17:09 -0500

    Sube cambios importantes, se recibe parámetro.

--- src/main/java/ec/com/gjijon/examples/kub/fintes/SampleResource.java
+++ src/main/java/ec/com/gjijon/examples/kub/fintes/SampleResource.java
@@ -1,8 +1,12 @@
 package ec.com.gjijon.examples.kub.fintes;
 
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -15,8 +19,14 @@ public class SampleResource {
   private String message;
 
   @GET
-  public Response message() {
-    return Response.ok(message).build();
+  @Produces(MediaType.TEXT_PLAIN)
+  @Path("{nombre}")
+  public Response message(
+		  @DefaultValue("SIN NOMBRE ESPECIFICADO")
+		  @PathParam("nombre")
+		  String nombre) {
+	  System.out.printf("Parametro de llegada: %s", nombre);
+    return Response.ok(message + nombre).build();
   }
 
 }

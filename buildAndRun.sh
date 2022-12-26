8db2c57b2f760d68d2b13a60c08800e63960db0e
gjijon <gjijon@gmail.com>
2022-12-26 15:00:48 -0500

    Sube codigo version 3, cambio a nombre de contenedor

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ $(docker ps -a -f name=fintes | grep -w fintes | wc -l) -eq 1 ]; then
-  docker rm -f gjijon/fintes
+  docker rm -f fintes
 fi
 mvn clean package && docker build -t gjijon/fintes .
-docker run -d -p 9080:9080 -p 9443:9443 --name gjijon/fintes gjijon/fintes
+docker run -d -p 9080:9080 -p 9443:9443 --name fintes gjijon/fintes
